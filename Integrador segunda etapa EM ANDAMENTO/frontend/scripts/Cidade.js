531d6338d80946fd541bebd4c7bad6c59fc879d5
CaioCSGandara <caiocsgandara@gmail.com>
2023-11-17 18:54:22 -0300

    alteracoes tabela cidade

--- Integrador segunda etapa EM ANDAMENTO/frontend/scripts/Cidade.js
+++ Integrador segunda etapa EM ANDAMENTO/frontend/scripts/Cidade.js
@@ -189,17 +189,26 @@ function RequisiçãoGETcidade() {
   }
   
   function preencherCidade(Cidade) {
+    let linha = 1;
     const tblBody = document.querySelector("tbody");
     Cidade.forEach((Cidade) => {
         const row = document.createElement("tr");
+        row.classList.add('tableHover');
+        if(linha%2!=0) {
+            row.classList.add('zebraOne');
+        }
+        else {
+            row.classList.add('zebraTwo');
+        }
         row.innerHTML = `
-            <td class="text-center align-middle padLeft" id="codigo">${Cidade.codigo}</td>
+            <td class=" padRow text-center align-middle padLeft" id="codigo">${Cidade.codigo}</td>
             <td class="text-center align-middle">${Cidade.nome}</td>
             <td class="text-center align-middle">${Cidade.uf}</td>
             <td class="text-center align-middle">${Cidade.pais}</td>
             <td class="align-middle"><img class="iconList" src="../images//lapisicon.png" onclick=" preencherAlterar(this, vetorIdsLabelCidade); exibeCodigo('${Cidade.codigo}', 'pcodAlter'); alternarDivs('divCadastrar', 'divAlterar')" ></td>
             <td class="align-middle"><img class="iconList" src="../images//lixeiraicon.png" onclick="limparStatus('statusCadastrar'); limparStatus('statusAlterar'); exibeCodigo('${Cidade.codigo}', 'pcodDelete'); popUpDeletar('${Cidade.codigo}')"></td>
         `;
+        linha = linha +1;
         tblBody.appendChild(row);
     });
   }
