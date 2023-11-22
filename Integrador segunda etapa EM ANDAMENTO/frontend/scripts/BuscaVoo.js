9fa27911ffd3d974ecb4ce9bb08fd785c847e984
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-11-22 13:11:09 -0300

    n

---
+++ Integrador segunda etapa EM ANDAMENTO/frontend/scripts/BuscaVoo.js
@@ -0,0 +1,55 @@
+function RequisiçãoGETaeroporto() {
+    const requestOptions = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+    };
+    return fetch('http://localhost:3000/listarAeroportos', requestOptions)
+      .then(T => T.json());
+  }
+
+
+function exibirAeroporto() {
+console.log('Entrou no exibir...');
+RequisiçãoGETaeroporto()
+    .then(customResponse => {
+    if (customResponse.status === "SUCCESS") {
+        console.log("Deu certo a busca de dados");
+        console.log('Payload:' + JSON.stringify(customResponse.payload));
+        preencherSelect(customResponse.payload, vetorDropdownOrigem, 'nome');
+        preencherSelect(customResponse.payload, vetorDropdownDestino, 'nome'); 
+    } else {
+        console.log(customResponse.message);
+    }
+    })
+    .catch((e) => {
+    console.log("Não foi possível exibir." + e);
+    });
+}
+
+function preencherSelect(options, vetor, casca) {
+    for (let i = 0; i < 2; i++) {
+        const selectDrop = document.getElementById(vetor[i]);
+
+        // Verifique se o elemento foi encontrado
+        if (!selectDrop) {
+            console.error(`Elemento com ID ${vetor[i]} não encontrado.`);
+            continue; // Pule para a próxima iteração do loop
+        }
+        
+      const defaultOption = document.createElement('option');
+      defaultOption.value = ''; 
+      defaultOption.text = 'Selecione uma opção';
+      selectDrop.appendChild(defaultOption);
+    
+      
+      options.forEach(optionValue => {
+        console.log("Código Cidade: " + JSON.stringify(optionValue));
+        const option = document.createElement('option');
+        option.value = optionValue.codigo; 
+        option.innerHTML = optionValue[casca]; 
+        selectDrop.appendChild(option);
+      });
+    }
+  }
+
+  
