fa5de74b02d62ba950109952bcce17a55097d8af
João Pedro Giaretta <Jpgoj2002@gmail.com>
2023-11-22 20:53:06 -0300

    Alterações na tela de busca voo

--- Integrador segunda etapa EM ANDAMENTO/frontend/scripts/BuscaVoo.js
+++ Integrador segunda etapa EM ANDAMENTO/frontend/scripts/BuscaVoo.js
@@ -66,5 +66,72 @@ function exibirAeroporto() {
 //    }
 //  }
 
+function RequisiçãoGETDados() {
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(body)
+  };
+  return fetch('http://localhost:3000/listarBuscaVoos', requestOptions)
+    .then(T => T.json());
+}
 
+async function selecionarVoos() {
+  const data = document.getElementById('dataIda').value;
+  const origem = document.getElementById('selectOrigemAeroportoCad').options[document.getElementById('selectOrigemAeroportoCad').selectedIndex].value;
+  const destino = document.getElementById('selectDestinoAeroportoCad').options[document.getElementById('selectDestinoAeroportoCad').selectedIndex].value;
+
+  await RequisiçãoGETDados({
+    data: data,
+    origem: origem,
+    destino: destino
+  })
+  .then(customResponse => {
+    if (customResponse.status === "SUCCESS") {
+      showStatusMessage("Busca realizada com sucesso.", false, "statusBusca");
+      exibirVoos();
+    } else{
+      showStatusMessage("Erro ao buscar voos: " + customResponse.message, true, "statusBusca");
+      console.log(customResponse.message);
+    }
+  })
+  .catch((e) => {
+    showStatusMessage("Erro técnico ao Buscar... Contate o suporte.", true, "statusBusca");
+    console.log("Não foi possível buscar." + e);
+  });
+}
+
+function exibirVoos() {
+  console.log('Entrou no exibir...');
+  RequisiçãoGETDados()
+    .then(customResponse => {
+      if (customResponse.status === "SUCCESS") {
+        console.log("Deu certo a busca de dados");
+        console.log('Payload:' + JSON.stringify(customResponse.payload));
+        preencherTabela(customResponse.payload, vetorTabela, 'nome');
+      } else {
+        console.log(customResponse.message);
+      }
+    })
+    .catch((e) => {
+      console.log("Não foi possível exibir." + e);
+    });
+}
   
+function preencherTabela(options, vetor, casca) {
+  const tblBody = document.getElementById('tBody');
+  Voo.forEach((Voo)=> {
+    const row = document.createElement('tr');
+    row.classList.add('tableHover');
+    row.innerHTML = `
+    <td id="codigo">${Voo.codigo}</td>
+    <td>${Voo.data}</td>
+    <td>${Voo.trecho}</td>
+    <td>${Voo.hr_saida}</td>
+    <td>${Voo.hr_chegada}</td>
+    <td>${Voo.origem}</td>
+    <td>${Voo.destino}</td>
+    <td>${Voo.valor}</td>`;
+    tblBody.appendChild(row);
+  })
+}
